ad23b4c6724e72184898351e819726885563e6ae
David88c <139010746+David88c@users.noreply.github.com>
2024-05-14 22:05:40 +0300

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Install Docker
 yum update -y
 amazon-linux-extras install -y docker
 service docker start
-
-# Pull the latest Nginx image from Docker Hub
-docker pull nginx:latest
+usermod -a -G docker ec2-user
